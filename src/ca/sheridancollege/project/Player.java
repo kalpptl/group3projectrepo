b245077879b2ef0891930261060e82c70493b5fb
User <User@LAPTOP-QVJ8BB4C.ht.home>
2022-06-14 09:48:18 -0400

    Written code of the game for player in player class.

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -6,6 +6,8 @@
  */
 package ca.sheridancollege.project;
 
+import java.util.ArrayList;
+
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  *
@@ -13,17 +15,21 @@ package ca.sheridancollege.project;
  * @author Paul Bonenfant
  * @author Prem Parashar
  */
-public abstract class Player {
+public class Player {
 
     private String name; //the unique name for this player
+    private GroupOfCards groupOfCards; // player's deck
+    int score; // players score
 
     /**
      * A constructor that allows you to set the player's unique ID
      *
      * @param name the unique ID to assign to this player.
      */
-    public Player(String name) {
+    public Player(String name, int numCardsPlayer, ArrayList<Card> cards) {
         this.name = name;
+        this.groupOfCards = new GroupOfCards(numCardsPlayer, cards);
+        this.score = 0;
     }
 
     /**
@@ -33,6 +39,8 @@ public abstract class Player {
         return name;
     }
 
+    public GroupOfCards getCards() {return groupOfCards;}
+    public int getScore() {return this.score;}
     /**
      * Ensure that the playerID is unique
      *
@@ -41,11 +49,19 @@ public abstract class Player {
     public void setName(String name) {
         this.name = name;
     }
+    public void setScore(int score1) {this.score = score1;}
 
     /**
      * The method to be overridden when you subclass the Player class with your specific type of Player and filled in
      * with logic to play your game.
      */
-    public abstract void play();
+//    public abstract void play();
+    @Override
+    public String toString() {
+        String ret = "Player: " + name + "\n";
+        ret += groupOfCards;
+        ret += "Score: " + score + "\n";
+        return ret;
+    }
 
 }
